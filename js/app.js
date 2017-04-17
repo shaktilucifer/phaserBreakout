605c0eba0ee424b8d20ed0bc86f53c72dc32ac4b
Shakti <shakticse@gmail.com>
2017-04-16 23:10:28 -0400

    playing with preload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    game.stage.backgroundColor = 'grey';
 }
 
 function create() {
