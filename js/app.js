bd02104e58756b72ae0f04232f43807ceca9c1fe
Shakti <shakticse@gmail.com>
2017-04-21 22:14:31 -0400

    Paddle and collision settings

--- js/app.js
+++ js/app.js
@@ -5,6 +5,9 @@ function preload() {
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = 'grey';
+    //TO DO add paddle image 
+    game.load.image('paddle', 'img/paddle.png');
+
 }
 
 function create() {
@@ -15,6 +18,15 @@ function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.enable(ball, Phaser.Physics.ARCADE);
     ball.body.velocity.set(150, 150);
+    ball.body.collideWorldBounds = true;
+    ball.body.bounce.set(1);
+    
+    //Paddle functions
+    
+    paddle = game.add.sprite(game.world.width*0.5, game.world.height-5, 'paddle');
+    paddle.anchor.set(0.5,1);
+    game.physics.enable(paddle, Phaser.Physics.ARCADE);
+    paddle.body.immovable = true;
     
 }
 
